42bb63a9d1f54b0177fae85e938900d3c96f0a2e
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-03-15 00:04:00 +0200

    change auth logic

--- pkg/auth/routes.go
+++ pkg/auth/routes.go
@@ -1,1 +1,22 @@
 package auth
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	_ "net/http"
+
+	_ "github.com/gorilla/mux"
+)
+
+func RegisterRouters(r *mux.Router) {
+	r.HandleFunc("/login", Login).Methods("POST")
+	r.HandleFunc("/register", Register).Methods("POST")
+}
+
+func Register(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func Login(writer http.ResponseWriter, request *http.Request) {
+
+}
