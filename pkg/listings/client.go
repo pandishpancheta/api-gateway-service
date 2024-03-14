bf61fd1c0bb26ae16f1f3b12220ae538b6584223
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-03-15 00:04:40 +0200

    make client

--- pkg/listings/client.go
+++ pkg/listings/client.go
@@ -1,1 +1,24 @@
 package listings
+
+import (
+	"fmt"
+	"github.com/pandishpancheta/api-gateway-service/pkg/config"
+	"github.com/pandishpancheta/api-gateway-service/pkg/listings/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type ServiceClient struct {
+	Client pb.ListingsServiceClient
+}
+
+func InitServiceClient(cfg *config.Config) (pb.ListingsServiceClient, error) {
+	creds := insecure.NewCredentials()
+	c, err := grpc.Dial(cfg.ListingsServiceAddress, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		fmt.Println("Error connecting to auth service:", err)
+		return nil, err
+	}
+
+	return pb.NewListingsServiceClient(c), nil
+}
