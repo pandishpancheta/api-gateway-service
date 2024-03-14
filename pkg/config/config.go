5a887414b181156d3158e8f97321480da492b40b
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-03-15 00:04:31 +0200

    add config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,29 @@
 package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port                   string `mapstructure:"PORT" default:"3001"`
+	ListingsServiceAddress string
+}
+
+func LoadConfig() (*Config, error) {
+	viper.AddConfigPath("envs/")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var config Config
+
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
