5262221c8ed7d654e4e8d303a65e3d9253ece983
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-03-15 00:04:50 +0200

    make routes for endpoints

--- pkg/listings/routes.go
+++ pkg/listings/routes.go
@@ -1,1 +1,49 @@
 package listings
+
+import (
+	"github.com/gorilla/mux"
+	_ "github.com/gorilla/mux"
+	"github.com/pandishpancheta/api-gateway-service/pkg/config"
+	"github.com/pandishpancheta/api-gateway-service/pkg/listings/routes"
+	"net/http"
+	_ "net/http"
+)
+
+func RegisterRouters(r *mux.Router, c *config.Config) *ServiceClient {
+	client, err := InitServiceClient(c)
+	if err != nil {
+		panic(err)
+	}
+	svc := &ServiceClient{
+		Client: client,
+	}
+
+	router := r.PathPrefix("/listings").Subrouter()
+	router.HandleFunc("/listings", svc.GetListings).Methods("GET")
+	router.HandleFunc("/listings", svc.CreateListing).Methods("POST")
+	router.HandleFunc("/listings/{id}", svc.GetListing).Methods("GET")
+	router.HandleFunc("/listings/{id}", svc.UpdateListing).Methods("PUT")
+	router.HandleFunc("/listings/{id}", svc.DeleteListing).Methods("DELETE")
+
+	return svc
+}
+
+func (svc *ServiceClient) DeleteListing(writer http.ResponseWriter, request *http.Request) {
+	routes.DeleteListing(writer, request, svc.Client)
+}
+
+func (svc *ServiceClient) UpdateListing(writer http.ResponseWriter, request *http.Request) {
+	routes.UpdateListing(writer, request, svc.Client)
+}
+
+func (svc *ServiceClient) GetListing(writer http.ResponseWriter, request *http.Request) {
+	routes.GetListing(writer, request, svc.Client)
+}
+
+func (svc *ServiceClient) CreateListing(writer http.ResponseWriter, request *http.Request) {
+	routes.CreateListing(writer, request, svc.Client)
+}
+
+func (svc *ServiceClient) GetListings(writer http.ResponseWriter, request *http.Request) {
+	routes.GetAllListings(writer, request, svc.Client)
+}
--- pkg/listings/routes/listings.go
+++ pkg/listings/routes/listings.go
@@ -1,1 +1,180 @@
 package routes
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/pandishpancheta/api-gateway-service/pkg/listings/pb"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"io/ioutil"
+	"net/http"
+)
+
+type UpdateListingRequest struct {
+	Name        string
+	Description string
+	Price       string
+	TagNames    []string
+}
+
+type UpdateStatusRequest struct {
+	Status string
+}
+
+func GetAllListings(w http.ResponseWriter, r *http.Request, c pb.ListingsServiceClient) {
+	res, err := c.GetListings(r.Context(), &emptypb.Empty{})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Write response as JSON to the writer
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetListing(w http.ResponseWriter, r *http.Request, c pb.ListingsServiceClient) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// Call the service
+	res, err := c.GetListing(r.Context(), &pb.GetListingRequest{Id: id})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Write response as JSON to the writer
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func CreateListing(w http.ResponseWriter, r *http.Request, c pb.ListingsServiceClient) {
+	createListingRequest := &pb.CreateListingRequest{}
+
+	// Decode the request body into the CreateListingRequest
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// cast the file to a byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	createListingRequest.Chunk = fileBytes
+	createListingRequest.Name = r.FormValue("name")
+	createListingRequest.Description = r.FormValue("description")
+	createListingRequest.Price = r.FormValue("price")
+
+	// TagNames is a repeated field, so we need to loop through the form values and add them to the request
+	for _, tag := range r.Form["tag"] {
+		createListingRequest.TagNames = append(createListingRequest.TagNames, tag)
+	}
+
+	// Call the service
+	res, err := c.CreateListing(r.Context(), createListingRequest)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Write response as JSON to the writer
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func UpdateListing(w http.ResponseWriter, r *http.Request, c pb.ListingsServiceClient) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var body UpdateListingRequest
+
+	// Decode the request body into the UpdateListingRequest
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Call the service
+	res, err := c.UpdateListing(r.Context(), &pb.UpdateListingRequest{
+		Id:          id,
+		Name:        body.Name,
+		Description: body.Description,
+		Price:       body.Price,
+		TagNames:    body.TagNames,
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Write response as JSON to the writer
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func UpdateListingStatus(w http.ResponseWriter, r *http.Request, c pb.ListingsServiceClient) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var body UpdateStatusRequest
+
+	// Decode the request body into the UpdateStatusRequest
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Call the service
+	res, err := c.UpdateListingStatus(r.Context(), &pb.UpdateListingStatusRequest{
+		Id:     id,
+		Status: body.Status,
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Write response as JSON to the writer
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func DeleteListing(w http.ResponseWriter, r *http.Request, c pb.ListingsServiceClient) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// Call the service
+	res, err := c.DeleteListing(r.Context(), &pb.DeleteListingRequest{Id: id})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Write response as JSON to the writer
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
