00aa54b5a25ac05c5c43e6142afba77f4970d6d8
Martin Bozhilov <marogo142005@gmail.com>
2024-03-15 11:19:33 +0200

    [ADD] main.go

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/pandishpancheta/api-gateway-service/pkg/auth"
+	"github.com/pandishpancheta/api-gateway-service/pkg/config"
+	"github.com/pandishpancheta/api-gateway-service/pkg/listings"
+	"github.com/pandishpancheta/api-gateway-service/pkg/order"
+)
+
 func main() {
 
+	cfg := config.LoadConfig()
+
+	r := mux.NewRouter()
+
+	authSvc := auth.RegisterRouters(r, cfg)
+
+	listings.RegisterRouters(r, cfg, authSvc.AuthClient)
+
+	order.RegisterRouters(r, cfg, authSvc.AuthClient)
+
+	// Start the server
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
