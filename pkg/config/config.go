0fe848ac4ed32219af78734f86f7789605058d76
Martin Bozhilov <marogo142005@gmail.com>
2024-03-16 05:30:33 +0200

    [FIX] CORS Headers

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -26,7 +26,8 @@ func AccessControlMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, r)
 	})
 }
