1bdec788603c1cdb1aa0f2914db302df790f0f0b
Martin Bozhilov <marogo142005@gmail.com>
2024-03-16 03:23:12 +0200

    Revert "[TEST] [FIX]"
    This reverts commit 5098d8cf477a337d7ed2729087b9e9b167983545.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -24,7 +24,7 @@ func LoadConfig() *Config {
 
 func AccessControlMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*, http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 		next.ServeHTTP(w, r)
